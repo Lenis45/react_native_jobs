5a1ae63d6b5fa1657ce428d852e191953cd498b9
Denis Kolesnikov <116916114+Lenis45@users.noreply.github.com>
2023-07-12 13:23:25 +0300

    setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+const Home = () => {
+    return (
+        <View>
+            <Text> Home </Text>
+        </View>
+    )
+}
+
+export default Home;
